9d82bfedf8cba1cf628121d1c3723f6ddc40833a
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2021-08-25 20:13:39 +0200

    Update vim-setup.sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# My vim setup
-
 mkdir -p ~/.vim/pack/vendor/start/
 mkdir -p ~/.vim/colors
 
@@ -14,6 +12,8 @@ git -C ~/.vim/pack/vendor/start/ clone https://github.com/ap/vim-buftabline
 git -C ~/.vim/pack/vendor/start/ clone https://github.com/vim-scripts/AutoComplPop
 git -C ~/.vim/pack/vendor/start/ clone https://github.com/vim-airline/vim-airline
 
+rm -rf ~/.vimrc
 mv .vimrc ~
 
 cd .. && rm -rf vim-setup/
+echo "Done!"
